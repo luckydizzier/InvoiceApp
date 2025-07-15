bede96fb29a239b76187cc79d6139e1eacba3ef3
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-15 02:35:45 +0200

    Add sample invoice item and lookup data creation

--- StartupOrchestrator.cs
+++ StartupOrchestrator.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.Json;
 using InvoiceApp.Data;
 using InvoiceApp.Models;
 using InvoiceApp.Repositories;
@@ -88,15 +90,107 @@ namespace InvoiceApp
                     var result = MessageBox.Show("Telepíti a mintaadatokat?", "Telepítés", MessageBoxButton.YesNo, MessageBoxImage.Question);
                     if (result == MessageBoxResult.Yes)
                     {
-                        var service = scope.ServiceProvider.GetRequiredService<IInvoiceService>();
+                        var invoiceService = scope.ServiceProvider.GetRequiredService<IInvoiceService>();
+                        var itemService = scope.ServiceProvider.GetRequiredService<IInvoiceItemService>();
+                        var productService = scope.ServiceProvider.GetRequiredService<IProductService>();
+                        var unitRepo = scope.ServiceProvider.GetRequiredService<IUnitRepository>();
+                        var groupRepo = scope.ServiceProvider.GetRequiredService<IProductGroupRepository>();
+                        var taxRepo = scope.ServiceProvider.GetRequiredService<ITaxRateRepository>();
+                        var logService = scope.ServiceProvider.GetRequiredService<IChangeLogService>();
+
+                        var createdInvoices = new List<Invoice>();
                         for (int i = 1; i <= 17; i++)
                         {
-                            await service.SaveAsync(new Invoice
+                            var inv = new Invoice
                             {
                                 Number = $"INV-{i:000}",
                                 Issuer = "Minta Kft.",
                                 Date = DateTime.Today.AddDays(-i),
                                 Amount = 100 + i
+                            };
+
+                            await invoiceService.SaveAsync(inv);
+                            createdInvoices.Add(inv);
+                        }
+
+                        var unit = new Unit
+                        {
+                            Code = "db",
+                            Name = "Darab",
+                            Active = true,
+                            DateCreated = DateTime.Now,
+                            DateUpdated = DateTime.Now
+                        };
+                        await unitRepo.AddAsync(unit);
+                        await logService.AddAsync(new ChangeLog
+                        {
+                            Entity = nameof(Unit),
+                            Operation = "Add",
+                            Data = JsonSerializer.Serialize(unit),
+                            DateCreated = DateTime.Now,
+                            DateUpdated = DateTime.Now,
+                            Active = true
+                        });
+
+                        var group = new ProductGroup
+                        {
+                            Name = "Általános",
+                            Active = true,
+                            DateCreated = DateTime.Now,
+                            DateUpdated = DateTime.Now
+                        };
+                        await groupRepo.AddAsync(group);
+                        await logService.AddAsync(new ChangeLog
+                        {
+                            Entity = nameof(ProductGroup),
+                            Operation = "Add",
+                            Data = JsonSerializer.Serialize(group),
+                            DateCreated = DateTime.Now,
+                            DateUpdated = DateTime.Now,
+                            Active = true
+                        });
+
+                        var tax = new TaxRate
+                        {
+                            Name = "ÁFA 27%",
+                            Percentage = 27,
+                            EffectiveFrom = DateTime.Today.AddYears(-1),
+                            Active = true,
+                            DateCreated = DateTime.Now,
+                            DateUpdated = DateTime.Now
+                        };
+                        await taxRepo.AddAsync(tax);
+                        await logService.AddAsync(new ChangeLog
+                        {
+                            Entity = nameof(TaxRate),
+                            Operation = "Add",
+                            Data = JsonSerializer.Serialize(tax),
+                            DateCreated = DateTime.Now,
+                            DateUpdated = DateTime.Now,
+                            Active = true
+                        });
+
+                        var product = new Product
+                        {
+                            Name = "Minta termék",
+                            Net = 100,
+                            Gross = 127,
+                            UnitId = unit.Id,
+                            ProductGroupId = group.Id,
+                            TaxRateId = tax.Id
+                        };
+                        await productService.SaveAsync(product);
+
+                        foreach (var inv in createdInvoices)
+                        {
+                            await itemService.SaveAsync(new InvoiceItem
+                            {
+                                InvoiceId = inv.Id,
+                                ProductId = product.Id,
+                                TaxRateId = tax.Id,
+                                Description = product.Name,
+                                Quantity = 1,
+                                UnitPrice = inv.Amount
                             });
                         }
                     }
