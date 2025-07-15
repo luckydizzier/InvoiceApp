7a7229e98614ad961bcdf694594af4d942a06ead
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-16 00:48:33 +0200

    Add invoice summary panel and edit header

---
+++ ViewModels/VatBreakdownEntry.cs
@@ -0,0 +1,30 @@
+
+namespace InvoiceApp.ViewModels
+{
+    public class VatBreakdownEntry : ViewModelBase
+    {
+        private decimal _rate;
+        private decimal _net;
+        private decimal _vat;
+
+        public decimal Rate
+        {
+            get => _rate;
+            set { _rate = value; OnPropertyChanged(); }
+        }
+
+        public decimal Net
+        {
+            get => _net;
+            set { _net = value; OnPropertyChanged(); }
+        }
+
+        public decimal Vat
+        {
+            get => _vat;
+            set { _vat = value; OnPropertyChanged(); }
+        }
+
+        public decimal Gross => Net + Vat;
+    }
+}
