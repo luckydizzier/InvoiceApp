adf94bdc63f72e20237170abb5287f276577d93d
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-17 01:13:49 +0200

    Add dashboard and invoice views

---
+++ Views/DashboardView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace InvoiceApp.Views
+{
+    public partial class DashboardView : UserControl
+    {
+        public DashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
