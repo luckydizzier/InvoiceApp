4b68a5bb3676214685b244f1c45547f1b34a9f52
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-17 10:53:11 +0200

    Update template selector for renamed AppState values

--- Helpers/AppStateTemplateSelector.cs
+++ Helpers/AppStateTemplateSelector.cs
@@ -11,18 +11,19 @@ namespace InvoiceApp.Helpers
     public class AppStateTemplateSelector : DataTemplateSelector
     {
         public DataTemplate? MainWindowTemplate { get; set; }
-        public DataTemplate? PaymentMethodTemplate { get; set; }
+        public DataTemplate? PaymentMethodViewTemplate { get; set; }
         public DataTemplate? SupplierTemplate { get; set; }
         public DataTemplate? ProductGroupTemplate { get; set; }
         public DataTemplate? TaxRateTemplate { get; set; }
         public DataTemplate? UnitTemplate { get; set; }
-        public DataTemplate? ProductTemplate { get; set; }
+        public DataTemplate? ProductViewTemplate { get; set; }
         public DataTemplate? DashboardTemplate { get; set; }
         public DataTemplate? InvoiceListTemplate { get; set; }
         public DataTemplate? InvoiceEditorTemplate { get; set; }
         public DataTemplate? InvoiceHeaderTemplate { get; set; }
         public DataTemplate? InvoiceItemsTemplate { get; set; }
         public DataTemplate? InvoiceSummaryTemplate { get; set; }
+        public DataTemplate? ConfirmDialogTemplate { get; set; }
 
         public override DataTemplate? SelectTemplate(object item, DependencyObject container)
         {
@@ -31,18 +32,19 @@ namespace InvoiceApp.Helpers
 
             return state switch
             {
-                AppState.PaymentMethodView => PaymentMethodTemplate,
+                AppState.PaymentMethodView => PaymentMethodViewTemplate,
                 AppState.Supplier => SupplierTemplate,
                 AppState.ProductGroup => ProductGroupTemplate,
                 AppState.TaxRate => TaxRateTemplate,
                 AppState.Unit => UnitTemplate,
-                AppState.ProductView => ProductTemplate,
+                AppState.ProductView => ProductViewTemplate,
                 AppState.Dashboard => DashboardTemplate,
                 AppState.InvoiceList => InvoiceListTemplate,
                 AppState.InvoiceEditor => InvoiceEditorTemplate,
                 AppState.InvoiceHeader => InvoiceHeaderTemplate,
                 AppState.InvoiceItems => InvoiceItemsTemplate,
                 AppState.InvoiceSummary => InvoiceSummaryTemplate,
+                AppState.ConfirmDialog => ConfirmDialogTemplate,
                 _ => MainWindowTemplate
             };
         }
