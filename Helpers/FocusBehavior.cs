e8c8528f0a9ab441ad27e667606e62b4e502b0cf
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-17 18:56:25 +0200

    Add AdvanceOnEnter behavior and apply to invoice header

--- Helpers/FocusBehavior.cs
+++ Helpers/FocusBehavior.cs
@@ -25,5 +25,66 @@ namespace InvoiceApp.Helpers
                 element.Focus();
             }
         }
+
+        public static readonly DependencyProperty AdvanceOnEnterProperty =
+            DependencyProperty.RegisterAttached(
+                "AdvanceOnEnter",
+                typeof(bool),
+                typeof(FocusBehavior),
+                new PropertyMetadata(false, OnAdvanceOnEnterChanged));
+
+        public static bool GetAdvanceOnEnter(DependencyObject obj) =>
+            (bool)obj.GetValue(AdvanceOnEnterProperty);
+
+        public static void SetAdvanceOnEnter(DependencyObject obj, bool value) =>
+            obj.SetValue(AdvanceOnEnterProperty, value);
+
+        public static readonly DependencyProperty EnterCommandOnLastProperty =
+            DependencyProperty.RegisterAttached(
+                "EnterCommandOnLast",
+                typeof(ICommand),
+                typeof(FocusBehavior),
+                new PropertyMetadata(null));
+
+        public static ICommand GetEnterCommandOnLast(DependencyObject obj) =>
+            (ICommand)obj.GetValue(EnterCommandOnLastProperty);
+
+        public static void SetEnterCommandOnLast(DependencyObject obj, ICommand value) =>
+            obj.SetValue(EnterCommandOnLastProperty, value);
+
+        private static void OnAdvanceOnEnterChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement element)
+            {
+                if ((bool)e.NewValue)
+                {
+                    element.PreviewKeyDown += ElementOnPreviewKeyDown;
+                }
+                else
+                {
+                    element.PreviewKeyDown -= ElementOnPreviewKeyDown;
+                }
+            }
+        }
+
+        private static void ElementOnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter || sender is not UIElement element)
+            {
+                return;
+            }
+
+            bool moved = element.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+            if (!moved)
+            {
+                var command = GetEnterCommandOnLast(element);
+                if (command?.CanExecute(null) == true)
+                {
+                    command.Execute(null);
+                }
+            }
+
+            e.Handled = true;
+        }
     }
 }
