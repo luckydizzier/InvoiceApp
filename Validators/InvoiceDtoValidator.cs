23aa96e4a53ca6d0a8def2aabe7135ee957d11f2
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-18 18:52:49 +0200

    Add validation for invoice items and update tests

--- Validators/InvoiceDtoValidator.cs
+++ Validators/InvoiceDtoValidator.cs
@@ -14,6 +14,9 @@ namespace InvoiceApp.Validators
             RuleFor(x => x.Amount).GreaterThanOrEqualTo(0);
             RuleFor(x => x.SupplierId).GreaterThan(0);
             RuleFor(x => x.PaymentMethodId).GreaterThan(0);
+            RuleFor(x => x.Items)
+                .Must(items => items?.Count > 0)
+                .WithMessage("Legal\u00e1bb egy t\u00e9tel sz\u00fcks\u00e9ges.");
             RuleForEach(x => x.Items).SetValidator(new InvoiceItemDtoValidator());
         }
     }
