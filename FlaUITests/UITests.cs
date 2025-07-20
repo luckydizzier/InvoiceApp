8fa3472baf34c48fd3dbce10da00e898965b6482
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-07-20 04:42:27 +0200

    Remove Appium tests and switch to FlaUI

--- FlaUITests/UITests.cs
+++ FlaUITests/UITests.cs
@@ -2,7 +2,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FlaUI.Core;
 using FlaUI.UIA3;
 using System;
-using System.Diagnostics;
 using System.IO;
 
 #nullable enable
@@ -13,31 +12,53 @@ namespace FlaUITests
     public class UITests
     {
         private const string HarnessExe = "UITestHarness.exe";
-        private Application? _application;
         private UIA3Automation? _automation;
 
         [TestInitialize]
         public void Setup()
         {
-            var exePath = Path.Combine(TestContext!.DeploymentDirectory, HarnessExe);
-            var psi = new ProcessStartInfo(exePath, "dialog");
-            _application = Application.Launch(psi);
             _automation = new UIA3Automation();
+        }
+
+        private Application LaunchHarness(string arguments = "")
+        {
+            var exePath = Path.Combine(TestContext!.DeploymentDirectory, HarnessExe);
+            return string.IsNullOrEmpty(arguments)
+                ? Application.Launch(exePath)
+                : Application.Launch(exePath, arguments);
         }
 
         [TestMethod]
         public void SampleDataDialogHasOkButton()
         {
-            var window = _application!.GetMainWindow(_automation!);
+            using var app = LaunchHarness("dialog");
+            var window = app.GetMainWindow(_automation!);
             var ok = window.FindFirstDescendant(cf => cf.ByAutomationId("OkButton"));
             Assert.IsNotNull(ok);
+        }
+
+        [TestMethod]
+        public void SampleDataDialogHasCancelButton()
+        {
+            using var app = LaunchHarness("dialog");
+            var window = app.GetMainWindow(_automation!);
+            var cancel = window.FindFirstDescendant(cf => cf.ByAutomationId("CancelButton"));
+            Assert.IsNotNull(cancel);
+        }
+
+        [TestMethod]
+        public void MainWindowHasNewInvoiceButton()
+        {
+            using var app = LaunchHarness();
+            var window = app.GetMainWindow(_automation!);
+            var newButton = window.FindFirstDescendant(cf => cf.ByAutomationId("NewInvoiceButton"));
+            Assert.IsNotNull(newButton);
         }
 
         [TestCleanup]
         public void Cleanup()
         {
             _automation?.Dispose();
-            _application?.Close();
         }
 
         public TestContext TestContext { get; set; } = null!;
